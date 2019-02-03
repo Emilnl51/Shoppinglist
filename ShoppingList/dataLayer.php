c447c91518584c4ee5c2d69ac00d5a7b6c8ffc84
EmilNL <emil.naslund@wiseware.se>
2019-02-03 22:06:19 +0100

    Tables Names are case sensitive in Raspbian

--- ShoppingList/dataLayer.php
+++ ShoppingList/dataLayer.php
@@ -20,7 +20,7 @@ class ListItem {
 }
 
 function getConnection() {
-    $connection = new PDO("mysql:dbname=shoppinglist;charset=utf8mb4;host=localhost", "root", "",
+    $connection = new PDO("mysql:dbname=ShoppingList;charset=utf8mb4;host=localhost", "root", "",
         array(
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_EMULATE_PREPARES => false
@@ -30,7 +30,7 @@ function getConnection() {
 }
 
 function getAllShoppingLists() {
-    $sql = sprintf("SELECT * FROM shoppinglist;");
+    $sql = sprintf("SELECT * FROM ShoppingList;");
     
     $statement = getConnection()->query($sql);
     $rows = $statement->fetchAll();
@@ -43,7 +43,7 @@ function getAllShoppingLists() {
 
 
 function getAllListItems($listId) {
-    $sql = "SELECT * FROM shoppinglist sl INNER JOIN listitem li ON sl.id = li.listId WHERE sl.id = :listId";
+    $sql = "SELECT * FROM ShoppingList sl INNER JOIN ListItem li ON sl.id = li.listId WHERE sl.id = :listId";
 
     $statement = getConnection()->prepare($sql);
     $statement->execute(array(':listId' => $listId));
@@ -56,7 +56,7 @@ function getAllListItems($listId) {
 }
 
 function insertItem($listId, $itemName) {
-    $sql = "INSERT INTO listItem (Id, ListId, ItemName) VALUES (null, :listId, :itemName);"; 
+    $sql = "INSERT INTO ListItem (Id, ListId, ItemName) VALUES (null, :listId, :itemName);"; 
     
     $statement = getConnection()->prepare($sql);
     $statement->execute(array(':listId' => $listId, ':itemName' => $itemName));
@@ -64,7 +64,7 @@ function insertItem($listId, $itemName) {
 }
 
 function deleteItem($listId, $itemId) {
-    $sql = "DELETE FROM listItem WHERE listId = :listId AND Id = :itemId";
+    $sql = "DELETE FROM ListItem WHERE listId = :listId AND Id = :itemId";
     
     $statement = getConnection()->prepare($sql);
     $statement->execute(array(':listId' => $listId, ':itemId' => $itemId));
@@ -72,14 +72,14 @@ function deleteItem($listId, $itemId) {
 }
 
 function updateItem($listId, $itemId, $itemName) {
-    $sql = "UPDATE listItem SET itemName = :itemName WHERE listId = :listId AND id = :itemId";
+    $sql = "UPDATE ListItem SET itemName = :itemName WHERE listId = :listId AND id = :itemId";
     
     $statement = getConnection()->prepare($sql);
     $statement->execute(array(':itemName' => $itemName, ':listId' => $listId, ':itemId' => $itemId));
 }
 
 function insertList($listName) {
-    $sql = "INSERT INTO shoppinglist (Id, ListName, ExternalId) VALUES (null, :listName, uuid());";
+    $sql = "INSERT INTO ShoppingList (Id, ListName, ExternalId) VALUES (null, :listName, uuid());";
     $conn = getConnection();
     $statement = $conn->prepare($sql);
     $statement->execute(array(':listName' => $listName));
@@ -88,15 +88,15 @@ function insertList($listName) {
 }
 
 function updateListName($listId, $listName) {
-    $sql = "UPDATE shoppinglist SET ListName = :listName WHERE id = :listId";
+    $sql = "UPDATE ShoppingList SET ListName = :listName WHERE id = :listId";
     
     $statement = getConnection()->prepare($sql);
     $statement->execute(array(':listName' => $listName, ':listId' => $listId));
 }
 
 function deleteList($listId) {
-    $sql = "DELETE FROM listItem WHERE listId = :listId";
-    $sql2 = "DELETE FROM shoppinglist WHERE id = :listId";
+    $sql = "DELETE FROM ListItem WHERE listId = :listId";
+    $sql2 = "DELETE FROM ShoppingList WHERE id = :listId";
     $statement = getConnection()->prepare($sql);
     $statement2 = getConnection()->prepare($sql2);
     $statement->execute(array(':listId' => $listId, ':listId' => $listId));
